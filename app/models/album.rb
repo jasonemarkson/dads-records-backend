e118aaa2fa921a54c70e03c6b4ae999910f92032
jasonemarkson <jasonmarkson76@gmail.com>
2022-02-05 15:18:07 -0500

    changes

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ApplicationRecord
     belongs_to :artist
     validates :title, presence: true, uniqueness: true
-    validates :year, presence: true
+    validates :year, presence: true, numericality: { only_integer: true }, length: { minimum: 4, maximum: 4 }
     validates :album_cover, presence: true
     validates :artist_id, presence: true
 end
